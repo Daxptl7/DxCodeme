184d8a780a428e212f465e7988e0c3129c6f488b
Daxptl7 <daxbharatbhai30@gmail.com>
2025-12-29 00:35:43 +0530

    Configure project for Vercel deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-!171@)rf7ip(^&v985tfw*u+or_w(qtle!#g@d(r_%)p8_$!+u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
